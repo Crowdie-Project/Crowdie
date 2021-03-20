1bcb0b66bc91992383895260bc701db6957e554f
madprogramer <themadprogramer@gmail.com>
2021-03-20 14:59:11 +0300

    Updated initial commit with bugfixes on ReportApplet
    Don't forget to add your .env!

--- Nav.js
+++ Nav.js
@@ -6,20 +6,25 @@ class Navig extends Component {
 
   constructor() {
     super();
+    this.locArr = [-1, -1, -1];
   }
   //need to check it again before submitting report to ensure user is where they tell they are
   getLocation() {
-    let locArr = [];
-      navigator.geolocation.getCurrentPosition(
-         position => {
-          locArr.push(position.coords.longitude);
-          locArr.push(position.coords.latitude);
-          locArr.push(position.timestamp);
-          console.log("Longitude is :", locArr[0]);
-          console.log("Latitude is :", locArr[1]);
-          console.log("Whole geolocation data:", position);
-       });  
-    return locArr;
+    navigator.geolocation.getCurrentPosition(
+      position => {
+      this.locArr[0] = position.coords.longitude;
+      this.locArr[1] = position.coords.latitude;
+      this.locArr[2] = position.timestamp;
+      //console.log("Whole geolocation data:", position);
+      this.showLocs();  
+    });
+    return this.locArr;
+  }
+
+  showLocs() {
+    console.log("Longitude is :", this.locArr[0]);
+    console.log("Latitude is :", this.locArr[1]);
+    console.log("Timestamp is :", this.locArr[2]);
   }
 }
 
