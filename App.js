c9302de1fefd2f16ce2a1fa0339c01354c9b34ca
madprogramer <themadprogramer@gmail.com>
2021-03-24 19:48:16 +0300

    Pushed final updates

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 //By the CrowdChain Team, 2021
 
 //IMPORTS
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { StyleSheet, View,ScrollView,Text } from 'react-native';
 import Report from './Components/Report';
-
+import {supabase} from './Components/Supabase.js';
 //ENVIRONMENT
 //import env from './config/env';
 //console.log(env);
@@ -14,12 +14,62 @@ import Report from './Components/Report';
 //MAIN
 
 export default function App() {
+  const [reports, setReports] = useState([]);
+  useEffect(() => {
+  
+    // let url = window.location.hash;
+    // let query = url.substr(1);
+    // let result = {};
 
+    // query.split("&").forEach((part) => {
+    //     const item = part.split("=");
+    //     result[item[0]] = decodeURIComponent(item[1]);
+    // });
+
+    // if (result.type === "recovery") {
+    //     setRecoveryToken(result.access_token);
+    // }
+
+    fetchReports().catch(console.error);
+}, []);
+
+  const fetchReports = async () => {
+    let { data: reports, error } = await supabase
+        .from("TestReports")
+        .select("*")
+        .order("id", { ascending: false });
+    if (error) console.log("error", error);
+    else setReports(reports);
+};
 
   return (
     <View style={styles.container}>
-        
-       <Report/>
+         <Report
+           reports={reports}
+           setReports={setReports}
+         />
+       <View style={styles.reportWrapper}>
+                 <Text style={styles.header}>Reported Events</Text>
+                   <ScrollView style={styles.scrollview}>
+               
+                    
+                    {reports.length ? (
+                        reports.map((report) => (
+                            <Text key={report.id} style={styles.reports}>
+                              code: {report.CODE} lat: {report.LAT} lon: {report.LON}
+                            </Text>
+                        ))
+                    ) : (
+                        <Text style={styles.reports}>
+                            You do have any reported events yet!
+                        </Text>
+                    )}
+              
+
+                </ScrollView>
+
+          </View>
+         
     </View>
     
   );
@@ -30,6 +80,21 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#D1D1D1',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
+    flexDirection: 'row'
+  },
+  reportWrapper: {
+    padding: 20
+  },
+  scrollview: {
+    height: 250
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 20
+  },
+  reports: {
+    fontSize: 14
   }
 });
