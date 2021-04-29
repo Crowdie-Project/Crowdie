1c2db6ef10810ea342ee842f695ffafb21b44a40
gkahraman16 <gkahraman16@ku.edu.tr>
2021-04-29 04:35:04 +0300

    Revert "marker clustering update"
    This reverts commit f3cad7a005c2b0541ed965b717dd2e29f59bb312.

--- Components/MapEditor.js
+++ Components/MapEditor.js
@@ -1,8 +1,7 @@
 //REACT IMPORTS
 import React, { useEffect, useRef, useState} from 'react';
 import {View, Text,TextInput, Button, StyleSheet,FlatList, requireNativeComponent, TouchableNativeFeedback, Dimensions} from 'react-native';
-import MapView from "react-native-map-clustering";
-import { Marker, Callout}from 'react-native-maps';
+import MapView, { Marker, Callout}from 'react-native-maps';
 import Navig from "./Nav";
 import MarkerDescription from "./MarkerDescription";
 //Navig instance for geolocation
@@ -13,11 +12,11 @@ const geoLoc = navig.getLocation();                                        //Loc
 const MapEditor = ({points,colors,filter}) => {
         
 return (
- 
+  
   <MapView
     style = {styles.mapEditor}
     loadingEnabled = {true}
-    initialRegion={{
+    region={{
       latitude: geoLoc[1],
       longitude: geoLoc[0],
       latitudeDelta: 0.0922,
@@ -27,13 +26,14 @@ return (
      {points.map((point) => (
     <Marker
       key={point.id}
-      coordinate={{latitude: point.LAT,longitude: point.LON}}
-    //  description={<MarkerDescription report={point}/>}
-     // pinColor={colors.filter(color => color.CategoryCode == point.CategoryCode).map(color => color.HexCode)[0]}
+      coordinate={[point.LAT,point.LON]}
+      description={<MarkerDescription report={point}/>}
+      pinColor={colors.filter(color => color.CategoryCode == point.CategoryCode).map(color => color.HexCode)[0]}
     />     
     
   ))}
   </MapView>
+
 );
 };
 
